764afa26a9fd22d5931d4fc3db1e255397f15911
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 19:03:38 +0400

    The Twelve Days of Christmas

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -1,9 +1,14 @@
 mod calc;
 use crate::calc::{celsius2farenheit, farenheit2celsius, fibonacci_loop, fibonacci_rec};
 
+mod song;
+use crate::song::twelve_days_of_christmas;
+
 fn main() {
     println!("celsius2farenheit: {}", celsius2farenheit(36));
     println!("farenheit2celsius: {}", farenheit2celsius(90));
     println!("fibonacci_loop:    {}", fibonacci_loop(5));
     println!("fibonacci_rec:     {}", fibonacci_rec(10));
+
+    twelve_days_of_christmas(12);
 }
