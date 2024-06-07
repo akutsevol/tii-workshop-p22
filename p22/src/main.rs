e163426178dfc4a0a2077a4bbfa827f580a0ebb7
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-08 01:10:29 +0400

    Initial commit

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -1,20 +1,3 @@
-mod figures;
-use crate::figures::{Shape,Point};
-
 fn main() {
-    let p = Shape::Point { p: Point {x: (1), y: (2)}};
-    let c = Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)};
-    let t = Shape::Triangle {
-            p1: (Point {x: 5, y: 5}),
-            p2: (Point {x: 10, y: 10}),
-            p3: (Point {x: 10, y: 5})
-        };
-    let rc= Shape::Rectangle {
-        p1: (Point {x: 15, y: 15}),
-        p2: (Point {x: 25, y: 25})
-    };
-    p.print();
-    c.print();
-    t.print();
-    rc.print();
+    println!("Hello world!");
 }
