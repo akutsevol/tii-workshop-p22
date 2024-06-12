206ade4965807ea1446b1fa12d182c170d22d1d9
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-12 18:37:16 +0400

    Fixing fmt issues

--- p22/src/figures.rs
+++ p22/src/figures.rs
@@ -1,17 +1,24 @@
-pub struct Point {pub x: u32, pub y: u32}
-
-pub enum Shape {
-    Point {p: Point},
-    Circle {p: Point, r: u32},
-    Triangle {p1: Point, p2: Point, p3: Point},
-    Rectangle {p1: Point, p2: Point},
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
 }
 
-impl Shape { 
+pub enum Shape {
+    Point { p: Point },
+    Circle { p: Point, r: u32 },
+    Triangle { p1: Point, p2: Point, p3: Point },
+    Rectangle { p1: Point, p2: Point },
+}
+
+impl Shape {
     fn area(&self) -> u64 {
         match *self {
             Shape::Circle { r, .. } => (std::f64::consts::PI * (r as f64).powi(2)) as u64,
-            Shape::Triangle { ref p1, ref p2, ref p3 } => {
+            Shape::Triangle {
+                ref p1,
+                ref p2,
+                ref p3,
+            } => {
                 let a = p1.distance_to(p2);
                 let b = p2.distance_to(p3);
                 let c = p3.distance_to(p1);
@@ -23,25 +30,29 @@ impl Shape {
                 let height = (p1.y as i64 - p2.y as i64).abs();
                 (width * height) as u64
             }
-            _ => 0
+            _ => 0,
         }
     }
 
     fn perimeter(&self) -> u64 {
         match *self {
             Shape::Circle { r, .. } => (2.0 * std::f64::consts::PI * r as f64) as u64,
-            Shape::Triangle { ref p1, ref p2, ref p3 } => {
+            Shape::Triangle {
+                ref p1,
+                ref p2,
+                ref p3,
+            } => {
                 let a = p1.distance_to(p2);
                 let b = p2.distance_to(p3);
                 let c = p3.distance_to(p1);
                 a + b + c
-            },
+            }
             Shape::Rectangle { ref p1, ref p2 } => {
                 let width = (p1.x as i64 - p2.x as i64).abs();
                 let height = (p1.x as i64 - p2.x as i64).abs();
                 (2 * (width + height)) as u64
             }
-            _ => 0
+            _ => 0,
         }
     }
 
@@ -53,26 +64,44 @@ impl Shape {
             Shape::Rectangle { .. } => "Rectangle".to_string(),
         }
     }
-    
+
     pub fn gen_string(&self) -> String {
         match self {
             Shape::Circle { p, r } => {
-    
-            format!("x: {} y: {} radius: {} area: {} perimeter: {}",
-                    p.x, p.y, r, self.area(), self.perimeter())
+                format!(
+                    "x: {} y: {} radius: {} area: {} perimeter: {}",
+                    p.x,
+                    p.y,
+                    r,
+                    self.area(),
+                    self.perimeter()
+                )
             }
             Shape::Triangle { p1, p2, p3 } => {
-                format!("x1: {} y1: {} x2: {} y2: {} x3: {} y3: {} area: {} perimeter: {}",
-                p1.x, p1.y,
-                p2.x, p2.y,
-                p3.x, p3.y,
-                self.area(), self.perimeter())
+                format!(
+                    "x1: {} y1: {} x2: {} y2: {} x3: {} y3: {} area: {} perimeter: {}",
+                    p1.x,
+                    p1.y,
+                    p2.x,
+                    p2.y,
+                    p3.x,
+                    p3.y,
+                    self.area(),
+                    self.perimeter()
+                )
             }
             Shape::Rectangle { p1, p2 } => {
-                format!("x1: {} y1: {} x2: {} y2: {}  area: {} perimeter: {}",
-                p1.x, p1.y, p2.x, p2.y, self.area(), self.perimeter())
+                format!(
+                    "x1: {} y1: {} x2: {} y2: {}  area: {} perimeter: {}",
+                    p1.x,
+                    p1.y,
+                    p2.x,
+                    p2.y,
+                    self.area(),
+                    self.perimeter()
+                )
             }
-            Shape::Point {p} => {
+            Shape::Point { p } => {
                 format!("x: {} y: {}", p.x, p.y)
             }
         }
@@ -94,26 +123,39 @@ mod tests {
     #[test]
     fn test_figures_names() {
         assert_eq!(
-            (Shape::Point { p: Point {x: (1), y: (2)}}).name().as_bytes(),
+            (Shape::Point {
+                p: Point { x: (1), y: (2) }
+            })
+            .name()
+            .as_bytes(),
             b"Point"
         );
         assert_eq!(
-            (Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)}).name().as_bytes(),
+            (Shape::Circle {
+                p: (Point { x: (10), y: (20) }),
+                r: (10)
+            })
+            .name()
+            .as_bytes(),
             b"Circle"
         );
         assert_eq!(
             (Shape::Triangle {
-                p1: (Point {x: 5, y: 5}),
-                p2: (Point {x: 10, y: 10}),
-                p3: (Point {x: 10, y: 5})
-            }).name().as_bytes(),
+                p1: (Point { x: 5, y: 5 }),
+                p2: (Point { x: 10, y: 10 }),
+                p3: (Point { x: 10, y: 5 })
+            })
+            .name()
+            .as_bytes(),
             b"Triangle"
         );
         assert_eq!(
             (Shape::Rectangle {
-                p1: (Point {x: 15, y: 15}),
-                p2: (Point {x: 25, y: 25})
-            }).name().as_bytes(),
+                p1: (Point { x: 15, y: 15 }),
+                p2: (Point { x: 25, y: 25 })
+            })
+            .name()
+            .as_bytes(),
             b"Rectangle"
         );
     }
@@ -121,26 +163,35 @@ mod tests {
     #[test]
     fn test_figures_areas() {
         assert_eq!(
-            (Shape::Point { p: Point {x: (1), y: (2)}}).area(),
+            (Shape::Point {
+                p: Point { x: (1), y: (2) }
+            })
+            .area(),
             0
         );
         assert_eq!(
-            (Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)}).area(),
+            (Shape::Circle {
+                p: (Point { x: (10), y: (20) }),
+                r: (10)
+            })
+            .area(),
             314
         );
         assert_eq!(
             (Shape::Triangle {
-                p1: (Point {x: 5, y: 5}),
-                p2: (Point {x: 10, y: 10}),
-                p3: (Point {x: 10, y: 5})
-            }).area(),
+                p1: (Point { x: 5, y: 5 }),
+                p2: (Point { x: 10, y: 10 }),
+                p3: (Point { x: 10, y: 5 })
+            })
+            .area(),
             8
         );
         assert_eq!(
             (Shape::Rectangle {
-                p1: (Point {x: 15, y: 15}),
-                p2: (Point {x: 25, y: 25})
-            }).area(),
+                p1: (Point { x: 15, y: 15 }),
+                p2: (Point { x: 25, y: 25 })
+            })
+            .area(),
             100
         );
     }
@@ -148,58 +199,81 @@ mod tests {
     #[test]
     fn test_figures_perimeters() {
         assert_eq!(
-            (Shape::Point { p: Point {x: (1), y: (2)}}).perimeter(),
+            (Shape::Point {
+                p: Point { x: (1), y: (2) }
+            })
+            .perimeter(),
             0
         );
         assert_eq!(
-            (Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)}).perimeter(),
+            (Shape::Circle {
+                p: (Point { x: (10), y: (20) }),
+                r: (10)
+            })
+            .perimeter(),
             62
         );
         assert_eq!(
             (Shape::Triangle {
-                p1: (Point {x: 5, y: 5}),
-                p2: (Point {x: 10, y: 10}),
-                p3: (Point {x: 10, y: 5})
-            }).perimeter(),
+                p1: (Point { x: 5, y: 5 }),
+                p2: (Point { x: 10, y: 10 }),
+                p3: (Point { x: 10, y: 5 })
+            })
+            .perimeter(),
             17
         );
         assert_eq!(
             (Shape::Rectangle {
-                p1: (Point {x: 15, y: 15}),
-                p2: (Point {x: 25, y: 25})
-            }).perimeter(),
+                p1: (Point { x: 15, y: 15 }),
+                p2: (Point { x: 25, y: 25 })
+            })
+            .perimeter(),
             40
         );
     }
 
     #[test]
     fn test_figures_point() {
-        let p = Shape::Point { p: Point {x: (1), y: (2)}};
+        let p = Shape::Point {
+            p: Point { x: (1), y: (2) },
+        };
         assert_eq!(p.gen_string().as_bytes(), b"x: 1 y: 2");
     }
 
     #[test]
     fn test_figures_circle() {
-        let c = Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)};
-        assert_eq!(c.gen_string().as_bytes(), b"x: 10 y: 20 radius: 10 area: 314 perimeter: 62");
+        let c = Shape::Circle {
+            p: (Point { x: (10), y: (20) }),
+            r: (10),
+        };
+        assert_eq!(
+            c.gen_string().as_bytes(),
+            b"x: 10 y: 20 radius: 10 area: 314 perimeter: 62"
+        );
     }
 
     #[test]
     fn test_figures_triangle() {
         let t = Shape::Triangle {
-            p1: (Point {x: 5, y: 5}),
-            p2: (Point {x: 10, y: 10}),
-            p3: (Point {x: 10, y: 5})
+            p1: (Point { x: 5, y: 5 }),
+            p2: (Point { x: 10, y: 10 }),
+            p3: (Point { x: 10, y: 5 }),
         };
-        assert_eq!(t.gen_string().as_bytes(), b"x1: 5 y1: 5 x2: 10 y2: 10 x3: 10 y3: 5 area: 8 perimeter: 17");
+        assert_eq!(
+            t.gen_string().as_bytes(),
+            b"x1: 5 y1: 5 x2: 10 y2: 10 x3: 10 y3: 5 area: 8 perimeter: 17"
+        );
     }
 
     #[test]
     fn test_figures_rectangle() {
-        let rc= Shape::Rectangle {
-            p1: (Point {x: 15, y: 15}),
-            p2: (Point {x: 25, y: 25})
+        let rc = Shape::Rectangle {
+            p1: (Point { x: 15, y: 15 }),
+            p2: (Point { x: 25, y: 25 }),
         };
-        assert_eq!(rc.gen_string().as_bytes(), b"x1: 15 y1: 15 x2: 25 y2: 25  area: 100 perimeter: 40");
+        assert_eq!(
+            rc.gen_string().as_bytes(),
+            b"x1: 15 y1: 15 x2: 25 y2: 25  area: 100 perimeter: 40"
+        );
     }
 }
--- p22/src/main.rs
+++ p22/src/main.rs
@@ -1,17 +1,22 @@
 mod figures;
-use crate::figures::{Shape,Point};
+use crate::figures::{Point, Shape};
 
 fn main() {
-    let p = Shape::Point { p: Point {x: (1), y: (2)}};
-    let c = Shape::Circle {p: (Point {x: (10), y: (20)}), r: (10)};
+    let p = Shape::Point {
+        p: Point { x: (1), y: (2) },
+    };
+    let c = Shape::Circle {
+        p: (Point { x: (10), y: (20) }),
+        r: (10),
+    };
     let t = Shape::Triangle {
-            p1: (Point {x: 5, y: 5}),
-            p2: (Point {x: 10, y: 10}),
-            p3: (Point {x: 10, y: 5})
-        };
-    let rc= Shape::Rectangle {
-        p1: (Point {x: 15, y: 15}),
-        p2: (Point {x: 25, y: 25})
+        p1: (Point { x: 5, y: 5 }),
+        p2: (Point { x: 10, y: 10 }),
+        p3: (Point { x: 10, y: 5 }),
+    };
+    let rc = Shape::Rectangle {
+        p1: (Point { x: 15, y: 15 }),
+        p2: (Point { x: 25, y: 25 }),
     };
     println!("{}: {}", p.name(), p.gen_string());
     println!("{}: {}", c.name(), c.gen_string());
